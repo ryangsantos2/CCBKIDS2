1b422ec793f64b92ab74a926bb4332320ab565ce
ryangsantos2 <ryan.santos@sptech.school>
2024-05-16 13:56:15 -0300

    erro do login solucionado

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,6 +1,33 @@
 var usuarioModel = require("../models/usuarioModel");
 
 
+// function autenticar(req, res) {
+//     var email = req.body.emailServer;
+//     var senha = req.body.senhaServer;
+
+//     if (email == undefined) {
+//         res.status(400).send("Seu email está undefined!");
+//     } else if (senha == undefined) {
+//         res.status(400).send("Sua senha está indefinida!");
+//     } else {
+
+//         usuarioModel.autenticar(email, senha)
+//             .then(
+//                 function (resultadoAutenticar) {
+//                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+//                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+//                 }
+//             ).catch(
+//                 function (erro) {
+//                     console.log(erro);
+//                     console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+//                     res.status(500).json(erro.sqlMessage);
+//                 }
+//             );
+//     }
+
+// }
+
 function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
@@ -16,6 +43,21 @@ function autenticar(req, res) {
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+                    if (resultadoAutenticar.length == 1) {
+                        console.log(resultadoAutenticar);
+                        res.json({
+                            id: resultadoAutenticar[0].id,
+                            cpf: resultadoAutenticar[0].cpf,
+                            email: resultadoAutenticar[0].email,
+                            nome: resultadoAutenticar[0].nome,
+
+                        })
+                    } else if (resultadoAutenticar.length == 0) {
+                        res.status(403).send("Email e/ou senha inválido(s)");
+                    } else {
+                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                    }
                 }
             ).catch(
                 function (erro) {
@@ -40,7 +82,7 @@ function cadastrar(req, res) {
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está indefinido!");
-    }else if (cpf == undefined) {
+    } else if (cpf == undefined) {
         res.status(400).send("Seu cpf está indefinido!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está indefinido!");
