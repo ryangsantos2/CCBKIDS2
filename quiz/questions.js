db4727bfbd9d45cfe383f8df3f655f0eeb426aee
ryangsantos2 <ryan.santos@sptech.school>
2024-05-20 23:35:05 -0300

    quiz pronto e oficialmente passos finais para o fim do site

---
+++ quiz/questions.js
@@ -0,0 +1,83 @@
+export default [
+    {
+        question: "Qual o primeiro livro da bíblia?",
+        answers: [
+            { option: "Josué", correct: false },
+            { option: "Gênesis", correct: true },
+            { option: "João", correct: false },
+        ],
+    },
+    {
+        question: "Após o diluvio, que animal foi enviado por noé para certificar que a água sobre a terra secou?",
+        answers: [
+            { option: "Corvo", correct: true },
+            { option: "Gaivota", correct: false },
+            { option: "Periquito", correct: false },
+        ],
+    },
+    {
+        question: "Na famosa historia, quem derrotou o gigante golias com uma pedra e estilingue?",
+        answers: [
+            { option: "Matheus", correct: false },
+            { option: "Davi", correct: true },
+            { option: "João", correct: false },
+        ],
+    },
+    {
+        question: "Jesus Cristo, tinha com ele 13 discipulos, que eram seus seguidores. Qual era o nome daquele conhecido como o traidor?",
+        answers: [
+            { option: "Pedro", correct: false },
+            { option: "Thiago", correct: false },
+            { option: "Judas", correct: true },
+        ],
+    },
+    {
+        question: "Qual o primeiro livro do novo testamento?",
+        answers: [
+            { option: "Matheus", correct: true },
+            { option: "Rute", correct: false },
+            { option: "Atos", correct: false },
+        ],
+    },
+    {
+        question: "Qual salmo pertence a passagem: ''O senhor é meu pastor, e nada me faltara''?",
+        answers: [
+            { option: "Salmos 37", correct: false },
+            { option: "Salmos 121", correct: false },
+            { option: "Salmos 23", correct: true },
+        ],
+    },
+    {
+        question: "Dentre os seguintes nomes, quem foi o homem conhecido por ter matado seu irmão??",
+        answers: [
+            { option: "Miquéias", correct: false },
+            { option: "Caim", correct: true },
+            { option: "Sadraque", correct: false },
+        ],
+    },
+    {
+        question: "Quem foi o responsavel por abrir o mar vermelho??",
+        answers: [
+            { option: "Moisés", correct: true },
+            { option: "Jesus", correct: false },
+            { option: "Daniel", correct: false },
+        ],
+    },
+    {
+        question: "No livro de Efésios, quantas pragas foram enviadas ao Egito?",
+        answers: [
+            { option: "10", correct: true },
+            { option: "5", correct: false },
+            { option: "7", correct: false },
+        ],
+    },
+    {
+        question: "Qual foi o homem conhecido por ser lançado na cova dos leões?",
+        answers: [
+            { option: "Josué", correct: false },
+            { option: "Matheus", correct: false },
+            { option: "Daniel", correct: true },
+        ],
+    }
+]
+
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -51,7 +51,8 @@ function autenticar(req, res) {
                             cpf: resultadoAutenticar[0].cpf,
                             email: resultadoAutenticar[0].email,
                             nome: resultadoAutenticar[0].nome,
-
+                            nomeFilho: resultadoAutenticar[0].nomeFilho,
+                            idadeFilho: resultadoAutenticar[0].idadeFilho,
                         })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
